bf2fecf1927db960d842f411730c0b4357af3644
tdpetrou <petrou.theodore@gmail.com>
2018-09-25 17:36:50 -0400

    during init make data a python array

--- my_array/__init__.py
+++ my_array/__init__.py
@@ -1,4 +1,5 @@
 from statistics import median
+from array import array
 
 class Array:
     '''
@@ -8,28 +9,69 @@ class Array:
     This array will compute lots of
     basic statistics.
 
-    Parameters
+    Attributes
     ----------
     data: list
         List of numbers
+
+    Methods
+    -------
+    TODO
     '''
 
-    
-    
+
     def __init__(self, data):
-        self.data = data
+        # b - boolean (1 byte integer)
+        # q - interger (4 bytes)
+        # d - float (8 bytes)
+        self.data = array('d', data)
 
     def sum(self):
+        '''
+        Sums all the values in the array
+
+        Returns
+        -------
+        int or float
+        '''
         return sum(self.data)
 
     def max(self):
+        '''
+        Finds max of all values in the array
+
+        Returns
+        -------
+        int or float
+        '''
         return max(self.data)
 
     def min(self):
+        '''
+        Finds min of all values in the array
+
+        Returns
+        -------
+        int or float
+        '''
         return min(self.data)
 
     def mean(self):
+        '''
+        Finds mean of all values in the array
+
+        Returns
+        -------
+        int or float
+        '''
         return self.sum() / len(self.data)
 
     def median(self):
+        '''
+        Finds median of all values in the array
+
+        Returns
+        -------
+        int or float
+        '''
         return median(self.data)
